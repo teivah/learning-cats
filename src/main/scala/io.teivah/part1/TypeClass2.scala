8f3e7c9fe81090917644f8d1853d5cfde1eeb011
teivah <teivah.io@gmail.com>
2018-08-01 22:37:35 +0200

    Override

--- src/main/scala/io.teivah/part1/TypeClass2.scala
+++ src/main/scala/io.teivah/part1/TypeClass2.scala
@@ -1,52 +1,45 @@
 package io.teivah.part1
 
-//sealed trait Result
-//
-//final case class ResultInt(v: Int) extends Result
-//
-//trait Computation[A] {
-//  def compute(v1: A, v2: A): Json
-//}
-//
-//// Singleton
-//object Result {
-//  def calculate[A](v1: A, v2: A)(implicit c: Computation[A]): Json = c.compute(v1, v2)
-//}
-//
-//object ResultInstances {
-//  implicit val intResult: Computation[Int] =
-//    (v1: Int, v2: Int) => ResultInt(v1 + v2)
-//}
-//
-//// Extension method
-//object JsonSyntax {
-//
-//  implicit class JsonWriterOps[A](value: A) {
-//    def toJson(implicit w: Computation[A]): Json = w.compute(value)
-//  }
-//
-//}
-//
-//object TypeClass2 {
-//  def main(args: Array[String]): Unit = {
-//    import JsonSyntax._
-//    import JsonWriterInstancesA._
-//    import ResultInstances._
-//
-//    val person = Json.toJson(Person("teivah", 30))
-//    println(person)
-//
-//    val int = Json.toJson(4)
-//    println(int)
-//
-//    val double = Json.toJson(4.1)
-//    println(double)
-//
-//    val json1 = Person("a", 1).toJson
-//    println(json1)
-//
-//    val json2 = 4.toJson
-//    println(json2)
-//  }
-//}
-//
+sealed trait Result
+
+final case class ResultInt(v: Int) extends Result
+
+trait Computation[A] {
+  def compute(v1: A)(v2: A): Result
+}
+
+// Singleton
+object Result {
+  def calculate[A](v1: A, v2: A)(implicit c: Computation[A]): Result = c.compute(v1)(v2)
+}
+
+object ResultInstances {
+  implicit val intResult: Computation[Int] =
+    new Computation[Int] {
+      override def compute(v1: Int)(v2: Int): Result = ResultInt(v1 + v2)
+    }
+}
+
+
+// Extension method
+object CalculationSyntax {
+
+  implicit class CalculationImpl[A](v1: A) {
+    def calculate2(v2: A)(implicit c: Computation[A]) = c.compute(v1)(v2)
+  }
+
+}
+
+object TypeClass2 {
+  def main(args: Array[String]): Unit = {
+    import CalculationSyntax._
+    import ResultInstances._
+
+    val result = Result.calculate(1, 2)
+    println(result)
+
+    val x = 1 calculate2 3
+    println(x)
+  }
+}
+
